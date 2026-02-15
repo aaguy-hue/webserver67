0b54db8f93cd5e2d17830e5ce253b81e4baf3364
aaguy-hue <aaditmathur6@gmail.com>
2026-02-15 18:46:39 +1100

    move error handling into macro

--- server.c
+++ server.c
@@ -8,6 +8,16 @@
 #include <unistd.h>
 #include <time.h>
 
+#define CHECK(x, msg) \
+	do { \
+		if ((x) < 0) { \
+			fprintf(stderr, "[%s:%d] %s failed", __FILE__, __LINE__, #x); \
+			perror(msg); \
+			status = EXIT_FAILURE; \
+			goto cleanup; \
+		} \
+	} while (0)
+
 // rfs 9110 is the modern rfc defining the http spec
 
 static const char *ADDRESS = "127.0.0.1";
@@ -17,75 +27,53 @@ static const int BACKLOG_LENGTH = 2;
 int main() {
 	int serverfd = -1;
 	int clientfd = -1;
+	int status = EXIT_SUCCESS;
 
 	serverfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (serverfd < 0) {
-		perror("[-] failed to create socket :(((");
-		exit(EXIT_FAILURE);
-	}
+	CHECK(serverfd, "[-] Failed to create socket :((");
 	printf("[+] yay our server socket was created!!!\n");
 
 	int enable = true;
 	int opt1ok = setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable));
 	int opt2ok = setsockopt(serverfd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(enable));
-	if (opt1ok < 0 || opt2ok < 0) {
-		perror("[-] Setting server socket options failed");
-		if (close(serverfd) < 0) {
-			perror("[--] Failed to close server socket!");
-		}
-		exit(EXIT_FAILURE);
-	}
+	CHECK(opt1ok, "[-] Setting server socket option SO_REUSEADDR failed");
+	CHECK(opt2ok, "[-] Setting server socket option SO_REUSEPORT failed");
 	printf("[+] set server socket options!\n");
 
 	struct sockaddr_in server_addr_in;
 	socklen_t addrlen = sizeof(server_addr_in);
 	memset(&server_addr_in, 0, addrlen);
 	server_addr_in.sin_family = AF_INET;
+	server_addr_in.sin_port = htons(PORT);
 	//server_addr_in.sin_addr.s_addr = INADDR_ANY; // this is just 0.0.0.0
 	int addrok = inet_pton(AF_INET, ADDRESS, &server_addr_in.sin_addr.s_addr);
 	if (addrok == 0) {
-		printf("[-] Invalid IP address passed into inet_pton!");
-		exit(EXIT_FAILURE);
-	} else if (addrok < 0) {
-		perror("[-] Failed to convert IP address to binary with inet_pton");
-		exit(EXIT_FAILURE);
+		printf("[-] Invalid IP address passed into inet_pton!\n");
+		status = EXIT_FAILURE;
+		goto cleanup;
 	}
-	server_addr_in.sin_port = htons(PORT);
+	CHECK(addrok, "[-] Failed to convert IP address to binary with inet_pton");
 
 	struct sockaddr *server_addr = (struct sockaddr *)&server_addr_in;
 
 	// bind server to the port
-	if (bind(serverfd, server_addr, addrlen) < 0) {
-		char errorstr[100];
-		snprintf(errorstr, sizeof(errorstr), "[-] failed to bind server socket to %s:%d", ADDRESS, PORT);
-		perror(errorstr);
-		if (close(serverfd) < 0) {
-			perror("[--] Failed to close server socket!");
-		}
-		exit(EXIT_FAILURE);
-	}
+
+	char errorstr[100];
+	snprintf(errorstr, sizeof(errorstr), "[-] failed to bind server socket to %s:%d", ADDRESS, PORT);
+	CHECK(bind(serverfd, server_addr, addrlen), errorstr);
+
 	char successtr[100];
 	snprintf(successtr, sizeof(successtr), "[+] succesfully bound server socket to %s:%d\n", ADDRESS, PORT);
 	printf(successtr);
 
-	if (listen(serverfd, BACKLOG_LENGTH) < 0) {
-		perror("[-] Failed to set server socket to listen for connections!");
-		if (close(serverfd) < 0) {
-			perror("[--] Failed to close server socket!");
-		}
-		exit(EXIT_FAILURE);
-	}
+	CHECK(listen(serverfd, BACKLOG_LENGTH), "[-] Failed to set server socket to listen for connections");
 	printf("[+] Server socket is now listening for connections!\n");
 
 	// Note: consider using accept4() on supporting platforms
 	struct sockaddr_in client_addr;
 	socklen_t addrsize = sizeof(client_addr);
 	clientfd = accept(serverfd, (struct sockaddr *)&client_addr, &addrsize);
-	if (clientfd < 0) {
-		perror("[-] Failed to accept client connection attempt");
-		close(serverfd);
-		exit(EXIT_FAILURE);
-	}
+	CHECK(clientfd, "[-] Failed to acccept client connection attempt");
 
 	char client_ip[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
@@ -111,4 +99,5 @@ cleanup:
 		}
 		printf("[+] Closed server socket!\n");
 	}
+	exit(status);
 }
