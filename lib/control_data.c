356380737bc74b176c8a5819af5f18b5acd49a8b
aaguy-hue <aaditmathur6@gmail.com>
2026-02-21 20:20:17 +1300

    fix crash

--- lib/control_data.c
+++ lib/control_data.c
@@ -18,7 +18,7 @@ RequestMethod getMethodFromStr(char* methodstr)
 	if (strcmp(methodstr, "HEAD") == 0)    return HEAD;
 	if (strcmp(methodstr, "OPTIONS") == 0) return OPTIONS;
 
-	printf("[-] Invalid HTTP method: %s\n", methodstr);
+	printf("[-] Invalid HTTP method: '%s'\n", methodstr);
 	exit(EXIT_FAILURE);
 }
 
@@ -28,15 +28,14 @@ ProtocolVersion getVersionFromStr(char* versionstr)
 	if (strcmp(versionstr, "HTTP/1.0") == 0) return HTTP10;
 	if (strcmp(versionstr, "HTTP/1.1") == 0) return HTTP11;
 
-	printf("[-] Invalid HTTP version: %s\n", versionstr);
+	printf("[-] Invalid HTTP version: '%s'\n", versionstr);
 	exit(EXIT_FAILURE);
 }
 
 ControlData getControlData(char** buf)
 {
 	size_t len = strcspn(*buf, "\n");
-	*buf[len] = '\0';
-	printf("hi: %d", (int)len);
+	(*buf)[len] = '\0';
 
 	char* line = *buf;
 
@@ -50,10 +49,10 @@ ControlData getControlData(char** buf)
 	strncpy(retval.target, token, REQUEST_TARGET_MAXLEN-1);
 	retval.target[REQUEST_TARGET_MAXLEN-1] = '\0';
 
-	token = strtok_r(line, " ", &line);
+	token = strtok_r(line, "\n", &line);
 	retval.version = getVersionFromStr(token);
 
-	*buf += len+1;
+	(*buf) += len+1;
 	return retval;
 }
 
